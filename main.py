0dd3bc5a84a183b2209ec45217c31b51edfe2193
GustavoDuarte-ufc <gustavoduarte2804@gmail.com>
2023-05-25 10:06:09 -0300

    Resolvido

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 
 def is_prime(number: int) -> bool:
     """Retorna True se o número for primo e False caso contrário."""
-    b = a
-    for i in range(2, a+1):
-        if a % i == 0:
+    b = number
+    for i in range(2, b+1):
+        if b % i == 0:
             b += 1
     if b != 1:
         return False
@@ -15,8 +15,9 @@ def is_prime(number: int) -> bool:
 
 def list_prime_factors(number: int) -> list[int]:
     """Retorna uma lista com os fatores primos de cada número da lista fornecida."""
-    c = a
+    c = number
     d = 2
+    lista = []
     while d < c+1:
         if c % d == 0:
             lista.append(d)
@@ -24,9 +25,4 @@ def list_prime_factors(number: int) -> list[int]:
             d = 2
         else:
             d+=1
-    print(lista)
-
-a = 11
-lista = []
-is_prime(a)
-list_prime_factors(lista)
+    return lista
